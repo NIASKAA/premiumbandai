3e1ca71ceb5fd58ed548a7c6b16fc6965f671fca
NIASKA <mkman751@yahoo.com>
2021-08-26 10:24:29 -0500

    added basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, split } from "@apollo/client";
+import {Navigation, Footer} from './Components';
+
+const httpLink = createHttpLink({
+  uri: "/graphql"
+})
+
+const getToken = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 function App() {
+  const client = new ApolloClient({
+    link: getToken.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
   return (
-    <div className="App">
-      
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navigation />
+            <Switch>
+              <Route exact path="/" component={Home}/>
+            </Switch>
+          <Footer/>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const Navigation = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navigation
