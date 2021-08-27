13fd4536c2969522318b73334b8480eeb81c4d65
NIASKA <mkman751@yahoo.com>
2021-08-26 20:19:47 -0500

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require("apollo-server-express")
-const {HighGrade, RealGrade, MasterGrade, PerfectGrade, Converge, SDGrade} = require('../models')
+const {HighGrade, RealGrade, MasterGrade, PerfectGrade, Converge, SDGrade, ProfileData} = require('../models')
 
 const resolvers = {
     Query: {
@@ -13,18 +13,46 @@ const resolvers = {
             return await MasterGrade.find({});
         },
         getPG: async () => {
-            return await HighGrade.find({});
+            return await PerfectGrade.find({});
         },
         getSD: async () => {
             return await SDGrade.find({});
         },
         getConverge: async () => {
             return await Converge.find({});
+        },
+        user: async (parent, args, context) => {
+            if(context.user) {
+                const user = await ProfileData.findById(context.user.id)
+                return user;
+            }
+            throw new AuthenticationError("Not Logged In")
         }
     },
 
     Mutation: {
-
+        addUser: async (parent, args) => {
+            try {
+                const user = await ProfileData.create(args)
+                const token = signToken(user)
+                return {token, user}
+            } catch (error) {
+                console.log(error)
+                throw new AuthenticationError("Username or Email Taken")
+            }
+        },
+        login: async (parent, {email, password}) => {
+            const user = await ProfileData.findOne({email})
+            if(!user) {
+                throw new AuthenticationError("Incorrect")
+            }
+            const correctPassword = await user.isCorrectPassword(password)
+            if(!correctPassword) {
+                throw new AuthenticationError("Incorrect")
+            }
+            const token = signToken(user)
+            return {token, user}
+        }
     }
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -61,6 +61,10 @@ const typeDefs = gql`
         releaseDate: Number
         price: Number
     }
+    type Auth {
+        _id: Int!
+        publicId: String!
+    }
 
     type Query {
         getHG: [HighGrade]
@@ -73,7 +77,8 @@ const typeDefs = gql`
     }
 
     type Mutation {
-        
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String): Auth
     }
 `;
 module.exports = typeDefs;
