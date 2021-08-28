9c5f95f08c7bcf85439cf57e5ae99a8aec6b1bd6
NIASKA <mkman751@yahoo.com>
2021-08-28 10:21:38 -0500

    initial styling of card

--- client/src/Components/ConvergeList/ConvergeCard/ConvergeCard.js
+++ client/src/Components/ConvergeList/ConvergeCard/ConvergeCard.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {Col, Card} from 'react-bootstrap'
+import './styles.css'
 
 const ConvergeCard = ({converge}) => {
     return (
         <>
             <Col>
                 <Card className="infoCard">
-                    <Card.Img src={converge.image} style={{ width: "17.9rem", height: "230px", marginBottom: '2%'}}></Card.Img>
-                    <p>{converge.gunplaName}</p>
+                    <Card.Img src={converge.image} style={{ width: "17.9rem"}}></Card.Img>
+                    <Card.Body className="infoContent">
+                        <h3 className="infoText">{converge.gunplaName}</h3>
+                        <p className="infoBody">Series: {converge.series}</p>
+                        <p className="infoBody">Release Date: {converge.releaseDate}</p>
+                        <p className="infoBody">Price: {converge.price} Yen</p>
+                    </Card.Body>
                 </Card>
             </Col>
         </>
