db32e7e2aad72f41c98a780310d4ad33c810b259
NIASKA <mkman751@yahoo.com>
2021-09-08 12:21:01 -0500

    test

--- server/seeders/mgSeeds.js
+++ server/seeders/mgSeeds.js
@@ -2,13 +2,6 @@ const {MasterGrade} = require("../models")
 
 const mgSeeds = [
     {
-        gunplaName: "Rx-78-2 Gundam Supreme Ver.",
-        image: "https://res.cloudinary.com/ddtqwizaf/image/upload/v1630183025/SUPREME-GUNDAM_ybt3re.jpg",
-        series: "Mobile Suit Gundam ",
-        releaseDate: "08-2021",
-        price: 0000
-    },
-    {
         gunplaName: 'Manuever Strike Pack',
         image: 'https://res.cloudinary.com/ddtqwizaf/image/upload/v1630183419/mg-maneuver-eclipse-gundam_1_lmxqld.jpg',
         series: "Gundam Seed Eclipse",
