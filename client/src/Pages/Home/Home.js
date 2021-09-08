b87eb4f6355f705875203909fb2723709e56e357
NIASKA <mkman751@yahoo.com>
2021-09-08 00:20:59 -0500

    styling homepage

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Carousel, Container} from 'react-bootstrap'
+import images from './images'
 import './styles.css'
 
 const Home = () => {
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+  
     return (
         <>
-            
+            <Container>
+                <h3>Premium-Bandai Gunpla Finder</h3>
+                <Carousel activeIndex={index} onSelect={handleSelect}>
+                    {images.map((id, image, caption, description) => {
+                    return (
+                        <Carousel.Item>
+                            <img key={id} src={image} alt={description} className="d-block w-100 h-60" />
+                            <Carousel.Caption>
+                                <h3>{caption}</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    );
+                    })}
+                </Carousel>
+            </Container>
         </>
     )
 }
