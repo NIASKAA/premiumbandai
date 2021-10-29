d1188faeaa8f80442572afe1bcb37a74643c7f09
NIASKA <mkman751@yahoo.com>
2021-10-29 12:44:28 -0500

    added donation button

--- client/src/Pages/FAQ/FAQ.js
+++ client/src/Pages/FAQ/FAQ.js
@@ -6,9 +6,10 @@ const FAQ = () => {
     return (
         <>
             <Container>
-                <h3 className="title">
+                <h3 className="titleFAQ">
                     What are Premium-Bandai's Gunpla?
                 </h3>
+
                 <p className="text">
                     Premium-Bandai's Gunpla are exclusive Gunplas that can only be ordered online through the 
                     official Premium-Bandai website that is dedicated for your region.
@@ -16,7 +17,7 @@ const FAQ = () => {
 
                 <br/>
 
-                <h3 className="title">
+                <h3 className="titleFAQ">
                     What does this website do?
                 </h3>
                 <p className="text">
@@ -28,8 +29,8 @@ const FAQ = () => {
 
                 <br/>
 
-                <h3 className="title">
-                    About Gundam Expos and Gundam Base Limiteds...
+                <h3 className="titleFAQ">
+                    About Gundam Expos and Gundam Base Limited...
                 </h3>
                 <p className="text">
                     You might wonder why those aren't included in the list. Well, those aren't Premium-Bandai's exclusives.
@@ -39,7 +40,7 @@ const FAQ = () => {
 
                 <br/>
 
-                <h3 className="title">
+                <h3 className="titleFAQ">
                     Will I consider adding Gundam Expos and Gundam Base Limited?
                 </h3>
                 <p className="text">
@@ -48,13 +49,29 @@ const FAQ = () => {
 
                 <br/>
 
-                <h3 className="title">
+                <h3 className="titleFAQ">
                     Hey, you missed a few Gunplas in your website! 
                 </h3>
                 <p className="text">
                     Sorry in advance if I do ever miss a few Gunplas. Please do email me about it and I will surely add it.
                 </p>
-                
+            
+                <br/>
+
+                <h3 className="titleFAQ">
+                    How can I support?
+                </h3>
+                <p className="text">
+                    At the moment, the servers are running out of my own pocket so if you want to help it alive, you are welcome to donate to my 
+                    paypal. Everything counts! Thank in advance! 
+                </p>
+                <form action="https://www.paypal.com/donate" method="post" target="_top">
+                <input type="hidden" name="business" value="SL4EJ7EZZWHV4" />
+                <input type="hidden" name="no_recurring" value="1" />
+                <input type="hidden" name="currency_code" value="USD" />
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
             </Container>
         </>
     )
